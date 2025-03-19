2f2a275569d7394f4ed05d6940c7508c2883a884
Aman Tiwari <amanvtiwari@gmail.com>
2025-03-19 15:22:02 +0530

    added assests, font and config setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
-
+import "./global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 export default function RootLayout() {
+  const [fontLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontLoaded) {
+      console.log("Fonts loaded");
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) return null;
   return <Stack />;
 }
